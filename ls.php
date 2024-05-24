02fd921ef5e3605073456ae129baf1d5bbd69358
Aurélio Soares <asc.mat@gmail.com>
2024-05-24 10:07:50 -0300

    cria arquivo para listar o conteúdo do diretório atual

---
+++ ls.php
@@ -0,0 +1,11 @@
+<?php
+
+$diretorioAtual = dir('.'); //Esse método com este parâmetro retorna um objeto do diretório atual.
+
+echo $diretorioAtual->path . PHP_EOL;
+
+echo '---------------------' . PHP_EOL;
+
+while ($arquivo = $diretorioAtual->read()) {
+    echo $arquivo . PHP_EOL;
+}
